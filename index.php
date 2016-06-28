4093a5ea69a46cd517eb3cb423e4b2877c66f687
CMOT <11280528@ittoluca.edu.mx>
2016-06-27 19:13:54 -0500

    subiendo pagina

---
+++ index.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+	<title>Pagina de inicio</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo_index.css">
+
+</head>
+<body>
+	<div id="nav">
+
+	</div>
+
+	
+
+	<div id="body">
+		<div id="wrapper">
+			<div id="header">
+				<img src="resources/LOGOTEC.PNG" >
+					<ul class="nav">
+						<li><a href="*">INICIO</a></li>
+
+						<li><a href="*">NOSOTROS</a></li>
+						<li><a href="*">OFERTA EDUCATIVA</a>
+							<ul>
+								<li><a href="*">MAESTRIAS</a></li>
+								<li><a href="*">Servicio</a></li>
+							</ul></li>
+						<li><a href="*">ESTUDIANTES</a></li>
+						<li><a href="*">GALERIA MULTIMEDIA</a></li>
+						<li></li>
+					</ul>
+			</div>
+			<div id="wrap_content">
+				<h1 id="title">Maestrias<span id="right-title">|</span></h1>
+				<hr>
+				<center>
+				<span class="item"><a href="*">Maestria en ciencias de la conducta</a></span>
+
+				<span class="item"><a href="pages/maestria_ciencias_computacion.html">Maestria en ciencias de la computacion</a></span>
+
+				<span class="item"><a href="*">Maestria en ciencias</a></span>
+				</center>
+			</div>
+		
+	</div>
+			
+		<div id="footer">
+			<div id="wrap_footer">
+				<div class="left-column columns">
+					<h3>Docentes</h3>
+					<hr>
+					<a href="">Correo institucional</a>
+
+
+				</div>
+				<div class="center-column columns">
+					<h3>Normatividad</h3>
+					<hr>
+					<a href="">Docentes</a>
+
+				</div>
+				<div class="right-column columns">
+					<h3>Enlace</h3>
+					<a href="">Enlancess</a>
+
+				</div>
+			</div>
+			<center>
+				<img src="resources/footerSEP.png">
+			<pre>Av. Tecnológico s/n. Colonia Agrícola Bellavista
+Metepec, Edo. De México, México C.P. 52149
+Tel. (52) (722) 2 08 72 00
+			</pre></center>
+
+
+		</div>
+
+
+
+</body>
+</html>
