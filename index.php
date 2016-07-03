2015998fbb976ada7a68f8280362595135bfbc08
CMOT <11280528@ittoluca.edu.mx>
2016-07-03 12:19:12 -0500

    subiendo maestria en ingenieras

--- index.php
+++ index.php
@@ -1,80 +1,149 @@
 <html>
-<head>
-	<title>Pagina de inicio</title>
-	<link rel="stylesheet" type="text/css" href="css/estilo_index.css">
+    <head>
+        <meta charset="utf-8">
+        <title>Pagina de inicio</title>
+
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css"/>
+        <link rel="stylesheet" type="text/css" href="css/estilo_index.css">
+        <script src="js/jquery.js" ></script>
+        <script src="js/bootstrap.min.js"/></script>
+
 
 </head>
 <body>
-	<div id="nav">
+    <header >
+        <div class="container">
+            <div class="inicio">
+                <a href="*">CONTACTO</a>
+                |
+                <a href="">INICIO</a>
 
-	</div>
+            </div>
+            <img src="resources/LOGOTEC.png" >
+        </div>
+    </header>
+    <nav id="row">
+        <div class="container">
+            <ul class="nav">
+                <li><a href="">INICIO</a></li>
 
-	
+                <li><a href="">NOSOTROS</a></li>
+                <li><a href="">OFERTA EDUCATIVA</a>
+                    <ul>
+                        <li><a href="">MAESTRIAS</a></li>
+                        <li><a href="">Servicio</a></li>
+                    </ul></li>
+                <li><a href="">ESTUDIANTES</a></li>
+                <li><a href="">GALERIA MULTIMEDIA</a></li>
+                <li><a href="">CALENDARIO ESCOLCAR 2015-2016</a></li>
+                <li><a href="">BUZÓN DE SUGERENCIAS</a></li>
+                
+            </ul>
+        </div>
+    </nav>
 
-	<div id="body">
-		<div id="wrapper">
-			<div id="header">
-				<img src="resources/LOGOTEC.PNG" >
-					<ul class="nav">
-						<li><a href="*">INICIO</a></li>
+    <section class="row">
+        <article class="container">
+            <article class="row titulo" >
+                <div class="col-lg-4 col-md-6 col-sm-10 col-xs-12" id='izq'>
+                    <h2>MAESTRÍA Y POSGRADO </h2>
+                </div>
+                <div class="col-lg-8 col-md-6 col-sm-2 col-xs-0 visible-lg visible-md visible-sm" id="der">
+                    <h2>A</h2>
+                </div>
 
-						<li><a href="*">NOSOTROS</a></li>
-						<li><a href="*">OFERTA EDUCATIVA</a>
-							<ul>
-								<li><a href="*">MAESTRIAS</a></li>
-								<li><a href="*">Servicio</a></li>
-							</ul></li>
-						<li><a href="*">ESTUDIANTES</a></li>
-						<li><a href="*">GALERIA MULTIMEDIA</a></li>
-						<li></li>
-					</ul>
-			</div>
-			<div id="wrap_content">
-				<h1 id="title">Maestrias<span id="right-title">|</span></h1>
-				<hr>
-				<center>
-				<span class="item"><a href="*">Maestria en ciencias de la conducta</a></span>
+            </article>
+            <hr>
+            <article >
 
-				<span class="item"><a href="pages/maestria_ciencias_computacion.html">Maestria en ciencias de la computacion</a></span>
+                <center>
+                    <a class="item" href="pages/maestria_ciencias_ingenieria.html"><span>Maestria en ciencias de la ingeniería</span></a>
 
-				<span class="item"><a href="*">Maestria en ciencias</a></span>
-				</center>
-			</div>
-		
-	</div>
-			
-		<div id="footer">
-			<div id="wrap_footer">
-				<div class="left-column columns">
-					<h3>Docentes</h3>
-					<hr>
-					<a href="">Correo institucional</a>
+                    <a class="item" href="pages/maestria_ciencias_ingenieria_ambiental.html"><span >Maestria en ciencias de la ingeniería ambiental</span></a>
 
+                    <a class="item" href="pages/doctorado_ciencias_ambientales.html"><span >Doctorado en ciencias ambientales</span></a>
+                </center>
 
-				</div>
-				<div class="center-column columns">
-					<h3>Normatividad</h3>
-					<hr>
-					<a href="">Docentes</a>
+            </article>
 
-				</div>
-				<div class="right-column columns">
-					<h3>Enlace</h3>
-					<a href="">Enlancess</a>
+        </article>
+    </section>
 
-				</div>
-			</div>
-			<center>
-				<img src="resources/footerSEP.png">
-			<pre>Av. Tecnológico s/n. Colonia Agrícola Bellavista
+    <footer class="row">
+        <section class="container">
+            <article class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                <h4>DOCENTES</h4>
+                <ul >
+                    <li><a href="*">Correo institucional</a></li>
+                    <li><a href="*">Accede al nuevo correo</a></li>
+                    <hr>
+                    <li><a href="*">Tutores Telemáticos</a></li>
+                    <li><a href="*">Captura de calificacione</a></li>
+                </ul>
+
+            </article>
+            <article class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                <div>
+                    <h4>NORMATIVIDAD</h4>
+                    <ul >
+                        <li><a href="*">Normativos TecNM</a></li>
+                        <li><a href="*">PIID TecNM</a></li>
+                        <hr>
+                        <li><a href="*">PIID ITToluca</a></li>
+                        <li><a href="*">PTA</a></li>
+                        <hr>
+                        <li><a href="">PIA</a></li>
+                    </ul>
+                </div>
+                <div class="gestion">
+                    <h4>GESTIÓN</h4>
+                    <ul >
+                        <li><a href="*">Calidad</a></li>
+                        <li><a href="*">Igualdad de género</a></li>
+
+                    </ul>
+                </div>
+            </article>
+            <article class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                <h4>ENLACE</h4>
+                <ul >
+                    <li><a href="*">SEP </a></li>
+                    <li><a href="*">TecNM </a></li>
+                    <hr>
+                    <li><a href="*">CONACYT </a></li>
+                    <li><a href="*">COMECYT </a></li>
+                    <hr>
+                    <li><a href="*">CONRICYT </a></li>
+                    <li><a href="*">Programa Delfín </a></li>
+                    <hr>
+                    <li><a href="*">Universia </a></li>
+                    <li><a href="*">ANUIES </a></li>
+                    <hr>
+                    <li><a href="*">Capítulo I. Electrónica</a></li>
+                    <li><a href="*">Capítulo I. Industrial</a></li>
+                    <hr>
+                    <li><a href="*">CESA </a></li>
+
+                </ul>
+
+            </article>
+        </section>
+        <section class="container">
+            <article>
+                <center>
+                    <img src="resources/footerSEP.png">
+                    <pre>Av. Tecnológico s/n. Colonia Agrícola Bellavista
 Metepec, Edo. De México, México C.P. 52149
 Tel. (52) (722) 2 08 72 00
-			</pre></center>
+                    </pre>
+                </center>
+            </article>
+        </section>
 
 
-		</div>
-
-
+    </footer>
 
 </body>
 </html>
+
